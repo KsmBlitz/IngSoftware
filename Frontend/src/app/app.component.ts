b3b821fa2955fda8ea80080d68de655145eae076
Vicente Estay <vjestayvaldivia@gmail.com>
2024-11-07 21:17:50 -0300

    Navbar Edit + Footer WIP+ Home WIP

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // Corregido aquí
 })
